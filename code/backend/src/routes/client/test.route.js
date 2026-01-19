026d3b8ce9310e5453d626f6c111b461d817af89
Yusariii <khailenguyen2004@gmail.com>
2026-01-19 22:46:54 +0700

    backend: test route update

--- code/backend/src/routes/client/test.route.js
+++ code/backend/src/routes/client/test.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const testController = require('../../controllers/client/test.controller');
+const { optionalAuth } = require('../../middlewares/auth.middleware');
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.get('/', testController.list);
 // GET /api/client/test/:id - Get test detail
 router.get('/:id', testController.detail);
 
-// POST /api/client/test/:id/submit - Submit test answers
-router.post('/:id/submit', testController.submit);
+// POST /api/client/test/:id/submit - Submit test answers (requires auth)
+router.post('/:id/submit', optionalAuth, testController.submit);
 
 module.exports = router;
